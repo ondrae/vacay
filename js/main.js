de54c8654e9d41e284cc39cf3fb562a21556f904
Andrew Hyder <hyd415@gmail.com>
2015-08-09 23:04:24 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+// INIT
+Parse.initialize("XQmd7CYYEys0WH2VqVh6nZzghFf3WXLUCAY7y11s", "IGJAkVBk1tZwWQ7VpCsFGnFj0OTfzstiGliaPGSQ");
+L.mapbox.accessToken = 'pk.eyJ1Ijoib25kcmFlIiwiYSI6IjhmODI5MjRmMDNkYWYxYzNjMzBmZGU0MGQ4NjQ2YjBjIn0.cVmAkqpsuP4BR9eAnz5hCw';
+
+// MAP
+$("#map").height(screen.height - 150);
+var map = L.mapbox.map('map', 'ondrae.n4ck2old', { zoomControl: false }).setView([37.77,-122.43],12);
+
+
+// SEARCH
+$("#search").submit(function(e){
+  e.preventDefault();
+  cid = "QCPOMMV0325RR5APTBIQFW5STNL1ZWR3TN2HFEFFELCPVBM1"
+  cs = "JII3J5ERSNQNXVXRRX10RBPOTLCLLBJWYDS50D4K5W1XKPOG"
+  q = $("#q").val();
+  fsq = "https://api.foursquare.com/v2/venues/search?client_id="+cid+"&client_secret="+cs+"&v=20130815&intent=global&query="+q
+  $.getJSON(fsq,
+    function(result){
+      place = result.response.venues[0]
+
+      // Zoom to top result
+      latlng = [place.location.lat,place.location.lng]
+      lnglat = [place.location.lng,place.location.lat]
+      map.setView(latlng,14)
+
+      L.mapbox.featureLayer({
+          type: 'Feature',
+          geometry: {
+              type: 'Point',
+              coordinates: lnglat
+          },
+          properties: {
+              'marker-size': 'large',
+              'marker-color': '#BE9A6B',
+              'marker-symbol': 'building'
+          }
+      }).addTo(map);
+
+      fsq_photo = "https://api.foursquare.com/v2/venues/" + place.id + "/photos?client_id="+cid+"&client_secret="+cs+"&v=20130815"
+      $.getJSON(fsq_photo,
+        function(result){
+          photo = result.response.photos.items[0];
+          url = photo.prefix + "300x300" + photo.suffix;
+          $("#place-photo").attr("src",url);
+
+          $("#card").removeClass("hidden").addClass("show");
+          $("#place-name").text( place.name );
+          $("#place-description").text( place.location.formattedAddress );
+          $("#card").animate({"top" : "-" + $("#card").height() - 300 }, 1000);
+
+        });
+
+    });
+
+    $("#close-panel").click(function(){
+      $("#card").animate({"top" : 0 }, 1000);
+    })
+});
